7ad67e64bc073fe4b2929cf33f9e9e5bdfa9ab00
yinyoushiren1 <107450984+yinyoushiren1@users.noreply.github.com>
2022-06-14 21:20:44 +0800

    push test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hf test");
         System.out.println("push test");
         System.out.println("push2 test");
+        System.out.println("push2 test");
     }
 }
