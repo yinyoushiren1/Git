fd2e5bacd895c4030d71a1671fc003d76379b848
LuJW <lujw981023@163.com>
2022-06-14 20:36:31 +0800

    提交信心

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author yhm
+ * @create 2022-06-14 20:26
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
